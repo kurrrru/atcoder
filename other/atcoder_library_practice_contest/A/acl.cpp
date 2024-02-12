a3423a2159e073097c250913cff1b732f57b6379
kurrrru <kurrrru8850@gmail.com>
2024-02-12 14:41:09 +0900

    acl contest A:AC

---
+++ other/atcoder_library_practice_contest/A/acl.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+
+int main()
+{
+	int N, Q, t, u, v;
+	cin >> N >> Q;
+	atcoder::dsu dsu(N);
+	for (int i = 0; i < Q; i++)
+	{
+		cin >> t >> u >> v;
+		if (t == 0)
+			dsu.merge(u, v);
+		else if (t == 1)
+			cout << (int)dsu.same(u, v) << endl;
+	}
+}
