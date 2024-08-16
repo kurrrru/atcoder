4772b886a989407299c8dc21225a3d6b4905f07b
kurrrru <kurrrru8850@gmail.com>
2024-08-16 17:33:48 +0900

    ABC283-E

--- ABC/251~300/283/E/main.cpp
+++ ABC/251~300/283/E/main.cpp
@@ -2,202 +2,6 @@
 using namespace std;
 using ll = long long;
 
-ll func(ll H, ll W, vector<vector<ll>> A)
-{
-	vector<bool> isolate(W, false);
-	isolate[0] = A[0][0] != A[0][1];
-	isolate[W - 1] = A[0][W - 1] != A[0][W - 2];
-	for (ll i = 1; i < W - 1; i++)
-	{
-		isolate[i] = A[0][i] != A[0][i - 1] && A[0][i] != A[0][i + 1];
-	}
-	ll ans = 0;
-	for (ll i = 1; i < H; i++)
-	{
-		ll cnt = 0;
-		for (ll j = 0; j < W; j++)
-		{
-			cnt += isolate[j];
-		}
-		if (cnt != 0)
-		{
-			ll cnt_front = 0;
-			ll cnt_back = 0;
-			if (i < H - 1)
-			{
-				for (ll j = 0; j < W; j++)
-				{
-					if (!isolate[j])
-					{
-						continue;
-					}
-					if (A[i][j] != A[i - 1][j])
-					{
-						cnt_front++;
-					}
-					else
-					{
-						cnt_back++;
-					}
-				}
-			}
-			else
-			{
-				vector<bool> isolate2(W, false);
-				isolate2[0] = A[i][0] != A[i][1];
-				isolate2[W - 1] = A[i][W - 1] != A[i][W - 2];
-				for (ll j = 1; j < W - 1; j++)
-				{
-					isolate2[j] = A[i][j] != A[i][j - 1] && A[i][j] != A[i][j + 1];
-				}
-				for (ll j = 0; j < W; j++)
-				{
-					if ((isolate[j] || isolate2[j]) && A[i][j] != A[i - 1][j])
-					{
-						cnt_front++;
-					}
-					else if ((isolate[j] || isolate2[j]) && A[i][j] == A[i - 1][j])
-					{
-						cnt_back++;
-					}
-				}
-			}
-			if (cnt_front > 0 && cnt_back > 0)
-			{
-				cout << -1 << endl;
-				return -1;
-			}
-			if (cnt_back == 0)
-			{
-				ans++;
-				for (ll j = 0; j < W; j++)
-				{
-					A[i][j] = 1 - A[i][j];
-				}
-			}
-		}
-		else
-		{
-			vector<bool> isolate2(W, false);
-			isolate2[0] = A[i][0] != A[i][1];
-			isolate2[W - 1] = A[i][W - 1] != A[i][W - 2];
-			for (ll j = 1; j < W - 1; j++)
-			{
-				isolate2[j] = A[i][j] != A[i][j - 1] && A[i][j] != A[i][j + 1];
-			}
-			ll cnt2 = 0;
-			for (ll j = 0; j < W; j++)
-			{
-				cnt2 += isolate2[j];
-			}
-			if (cnt2 != 0)
-			{
-				if (i == H - 1)
-				{
-					ll cnt_front = 0;
-					ll cnt_back = 0;
-					for (ll j = 0; j < W; j++)
-					{
-						if (!isolate2[j])
-						{
-							continue;
-						}
-						if (A[i][j] != A[i - 1][j])
-						{
-							cnt_front++;
-						}
-						else
-						{
-							cnt_back++;
-						}
-					}
-					if (cnt_front > 0 && cnt_back > 0)
-					{
-						cout << -1 << endl;
-						return -1;
-					}
-					if (cnt_front > 0)
-					{
-						ans++;
-						for (ll j = 0; j < W; j++)
-						{
-							A[i][j] = 1 - A[i][j];
-						}
-					}
-				}
-				else
-				{
-					ll cnt_front = 0;
-					ll cnt_back = 0;
-					for (ll j = 0; j < W; j++)
-					{
-						if (!isolate2[j])
-						{
-							continue;
-						}
-						if (A[i][j] != A[i + 1][j])
-						{
-							cnt_front++;
-						}
-						else
-						{
-							cnt_back++;
-						}
-					}
-					if (cnt_front > 0 && cnt_back > 0)
-					{
-						ll cnt_front2 = 0;
-						ll cnt_back2 = 0;
-						for (ll j = 0; j < W; j++)
-						{
-							if (!isolate2[j])
-							{
-								continue;
-							}
-							if (A[i][j] != A[i - 1][j])
-							{
-								cnt_front2++;
-							}
-							else
-							{
-								cnt_back2++;
-							}
-						}
-						if (cnt_front2 > 0 && cnt_back2 > 0)
-						{
-							cout << -1 << endl;
-							return -1;
-						}
-						if (cnt_front2 > 0)
-						{
-							ans++;
-							for (ll j = 0; j < W; j++)
-							{
-								A[i][j] = 1 - A[i][j];
-							}
-						}
-					}
-				}
-			}
-		}
-		isolate[0] = A[i][0] != A[i][1] && A[i][0] != A[i - 1][0];
-		isolate[W - 1] = A[i][W - 1] != A[i][W - 2] && A[i][W - 1] != A[i - 1][W - 1];
-		for (ll j = 1; j < W - 1; j++)
-		{
-			isolate[j] = A[i][j] != A[i][j - 1] && A[i][j] != A[i][j + 1] && A[i][j] != A[i - 1][j];
-		}
-	}
-	for (ll i = 0; i < W; i++)
-	{
-		if (isolate[i])
-		{
-			cout << -1 << endl;
-			return -1;
-		}
-	}
-	return ans;
-}
-
 int main()
 {
 	ll H, W;
@@ -210,15 +14,95 @@ int main()
 			cin >> A[i][j];
 		}
 	}
-	ll ans = func(H, W, A);
-	if (ans == -1)
+	constexpr ll INF = 1ll << 60;
+	vector<vector<vector<ll>>> dp(H, vector<vector<ll>>(2, vector<ll>(2, INF)));
+	dp[0][0][0] = 0;
+	dp[0][0][1] = 1;
+	dp[0][1][0] = 0;
+	dp[0][1][1] = 1;
+	for (ll i = 1; i < H; i++)
 	{
-		return 0;
+		ll cnt_isolated_ff = 0;
+		ll cnt_isolated_fb = 0;
+		ll cnt_isolated_bf = 0;
+		ll cnt_isolated_bb = 0;
+		for (ll l = 0; l < W; l++)
+		{
+			if ((l != 0 && A[i - 1][l - 1] == A[i - 1][l]) || (l != W - 1 && A[i - 1][l] == A[i - 1][l + 1]))
+			{
+				continue;
+			}
+			if ((i - 1 == 0 || A[i - 2][l] != A[i - 1][l]) && (A[i - 1][l] != A[i][l]))
+			{
+				cnt_isolated_ff++;
+			}
+			if ((i - 1 == 0 || A[i - 2][l] != A[i - 1][l]) && (A[i - 1][l] == A[i][l]))
+			{
+				cnt_isolated_fb++;
+			}
+			if ((i - 1 == 0 || A[i - 2][l] == A[i - 1][l]) && (A[i - 1][l] != A[i][l]))
+			{
+				cnt_isolated_bf++;
+			}
+			if ((i - 1 == 0 || A[i - 2][l] == A[i - 1][l]) && (A[i - 1][l] == A[i][l]))
+			{
+				cnt_isolated_bb++;
+			}
+		}
+		if (cnt_isolated_ff == 0)
+		{
+			dp[i][0][0] = min(dp[i][0][0], dp[i - 1][0][0]);
+			dp[i][1][1] = min(dp[i][1][1], dp[i - 1][1][1] + 1);
+		}
+		if (cnt_isolated_bf == 0)
+		{
+			dp[i][0][0] = min(dp[i][0][0], dp[i - 1][1][0]);
+			dp[i][1][1] = min(dp[i][1][1], dp[i - 1][0][1] + 1);
+		}
+		if (cnt_isolated_fb == 0)
+		{
+			dp[i][0][1] = min(dp[i][0][1], dp[i - 1][0][0] + 1);
+			dp[i][1][0] = min(dp[i][1][0], dp[i - 1][1][1]);
+		}
+		if (cnt_isolated_bb == 0)
+		{
+			dp[i][0][1] = min(dp[i][0][1], dp[i - 1][1][0] + 1);
+			dp[i][1][0] = min(dp[i][1][0], dp[i - 1][0][1]);
+		}
+		if (dp[i][0][0] == INF && dp[i][0][1] == INF && dp[i][1][0] == INF && dp[i][1][1] == INF)
+		{
+			cout << -1 << endl;
+			return 0;
+		}
 	}
+	ll ans = INF;
+	ll cnt_isolated_ff = 0;
+	ll cnt_isolated_fb = 0;
 	for (ll i = 0; i < W; i++)
 	{
-		A[0][i] = 1 - A[0][i];
+		if ((i != 0 && A[H - 1][i - 1] == A[H - 1][i]) || (i != W - 1 && A[H - 1][i] == A[H - 1][i + 1]))
+		{
+			continue;
+		}
+		if (A[H - 2][i] != A[H - 1][i])
+		{
+			cnt_isolated_ff++;
+		}
+		if (A[H - 2][i] == A[H - 1][i])
+		{
+			cnt_isolated_fb++;
+		}
 	}
-	ll ans2 = func(H, W, A) + 1;
-	cout << min(ans, ans2) << endl;
+	if (cnt_isolated_ff == 0)
+		ans = min(ans, min(dp[H - 1][0][0], dp[H - 1][1][1]));
+	if (cnt_isolated_fb == 0)
+		ans = min(ans, min(dp[H - 1][0][1], dp[H - 1][1][0]));
+	if (ans == INF)
+	{
+		cout << -1 << endl;
+	}
+	else
+	{
+		cout << ans << endl;
+	}
 }
